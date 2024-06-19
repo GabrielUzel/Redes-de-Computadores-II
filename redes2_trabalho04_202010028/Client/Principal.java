bbebe3be2a9c4a0923b8e3fae51c021e01949861
GabrielUzel <gabrieluzel1@gmail.com>
2024-06-19 15:41:13 -0300

    More testing

--- redes2_trabalho04_202010028/Client/Principal.java
+++ redes2_trabalho04_202010028/Client/Principal.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {
-        Scanner readTerminal = new Scanner(System.in);
-
         Client client = new Client();
         client.startClient();
     }
